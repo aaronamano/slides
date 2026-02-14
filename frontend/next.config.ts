fb4b9aeb0cc33a2aea2d458cdef13674a1536dd8
aaronamano <aaroncedric2005@yahoo.com>
2026-02-14 02:08:18 -0500

    added production fastapi url env var

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   env: {
     KIBANA_URL: process.env.KIBANA_URL,
     ELASTICSEARCH_API_KEY: process.env.ELASTICSEARCH_API_KEY,
+    FASTAPI_URL: process.env.FASTAPI_URL
   },
 };
 
